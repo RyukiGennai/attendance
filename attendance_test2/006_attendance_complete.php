c50fa176c5e7f16b6727aec8427d0cb7c7ce8ba8
RyukiGennai <K023C0063@g.neec.ac.jp>
2026-01-08 10:57:26 +0900

    細かい修正

--- attendance_test2/006_attendance_complete.php
+++ attendance_test2/006_attendance_complete.php
@@ -11,10 +11,5 @@ require_once 'header.php';
 <div class="max-w-md w-full bg-white p-8 rounded shadow text-center mx-auto mt-20">
     
     <h2 class="text-2xl font-bold mb-4 text-green-600">出席完了</h2>
-    
-    <p class="mb-8"><?= htmlspecialchars($_SESSION['name'] ?? '') ?></p>
-    
-    <a href="005_student_dashboard.php" class="block w-full bg-blue-600 text-white p-3 rounded">
-        トップへ戻る
-    </a>
+    <a href="005_student_dashboard.php" class="block w-full bg-blue-600 text-white p-3 rounded">トップへ戻る</a>
 </div>
